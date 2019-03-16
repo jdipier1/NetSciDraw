3e6474483d2511d785f682286f81adebd5050c32
jheller9 <jheller9@binghamton.edu>
2019-03-16 19:05:43 -0400

    zoom / drag fuctionality, shape bugfixes

--- v3.1/js/Mouse.js
+++ v3.1/js/Mouse.js
@@ -68,11 +68,12 @@ Mouse.init = function(target){
 			e.preventDefault();
 		  
 			if (e.ctrlKey) {
-			 	Model.targetScale += e.deltaY * 0.025;
+				 Model.targetScale += e.deltaY * 0.025;
+				 Model.targetScale = Math.min(Math.max(Model.targetScale, 0.25), 1);	
 			} else {
 				// Your trackpad X and Y positions
-			  	Model.xOffset -= e.deltaX * 30;
-			  	Model.yOffset -= e.deltaY * 30;
+			  	Model.xOffset += e.deltaX * 30;
+			  	Model.yOffset += e.deltaY * 30;
 			}
 		  
 			//Mouse.onPinchMove();
@@ -117,7 +118,7 @@ Mouse.onPinchMove = function(e) {
 		e.touches[0].pageX - e.touches[1].pageX,
 		e.touches[0].pageY - e.touches[1].pageY);
 
-	Model.targetScale = Math.min(Math.max(Mouse.initPinchScale + ((Mouse.initPinchDist - dist)/Mouse.PINCH_SENSITIVITY), .25), 3);	
+	Model.targetScale = Math.min(Math.max(Mouse.initPinchScale + ((Mouse.initPinchDist - dist)/Mouse.PINCH_SENSITIVITY), 0.25), 1);	
 }
 
 Mouse.onPinchEnd = function(e) {
